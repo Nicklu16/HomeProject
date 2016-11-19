58e52d7132fac2779aa24a2b436d20a27d8ed40a
Nicklu16 <Nicklu16@users.noreply.github.com>
2016-11-19 23:34:58 +0300

    Update lineur.java

--- src/lineur.java
+++ src/lineur.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class lineur {
 
     public static void main(String[] args) {
         System.out.println("Решаем уравнение типа ax + b = 0");
