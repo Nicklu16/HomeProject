3c7d5627554720f760d3d1473639e7a305a5580d
Nicklu16 <Nicklu16@users.noreply.github.com>
2016-11-24 17:47:55 +0300

    Таблица умножения

---
+++ src/mtable.java
@@ -0,0 +1,14 @@
+public class mtable {
+    public static void main(String[] args) {
+        int a, b;
+        for (a = 1; a < 10; a++)
+            for (a = 1; a < 10; a++) {
+
+                for (b = 1; b < 10; b++) {
+                    System.out.printf("%2d ", a * b);
+                }
+                System.out.printf("\n");
+
+            }
+    }
+}
