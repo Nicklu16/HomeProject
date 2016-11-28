440067d54e346a87cd1d13ee6ab194f2325b6011
Nicklu16 <Nicklu16@users.noreply.github.com>
2016-11-28 16:53:40 +0300

    Random int mass

---
+++ src/random.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class random {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int a[] = new int[15];
+        for (int i = 0; i < 15; i++) {
+            a[i]= random.nextInt(150);
+            System.out.println(a[i]);
+
+        }
+    }
+}
