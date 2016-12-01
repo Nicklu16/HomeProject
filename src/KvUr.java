20946df82b4c31f3fbd194e894593c49d814da96
Nicklu16 <Nicklu16@users.noreply.github.com>
2016-12-01 14:36:13 +0300

    Квадратное уравнение // задание D

--- src/KvUr.java
+++ src/KvUr.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class KvUr2 {
+public class KvUr {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double a = scanner.nextDouble();
